3c669809eeefa0e6770d091477d5aaa10a873571
JoDaBaRo <josdabayona@gmail.com>
2019-03-29 15:22:01 -0500

    implements modal in user admin form

--- app/javascript/packs/sign_up.js
+++ app/javascript/packs/sign_up.js
@@ -50,8 +50,65 @@ document.addEventListener('turbolinks:load', () => {
         else {
           el.classList.replace('invalid_form', 'focus_form')  
         }
-      }
+      }, 
+        close(i) {
+            var a = document.getElementsByClassName('element')[i];
+            if(a.scrollHeight == 0){
+              Vue.set(this.show, i, this.show[i] =! this.show[i]);
+              setTimeout(function(){a.style.maxHeight = a.scrollHeight + "px"}, 30)
+              document.getElementById(`transform_collapse_${i}`).classList.remove('transform_collapse_2');
+              document.getElementById(`transform_collapse_${i}`).classList.add('transform_collapse');
+            }else{
+              a.style.maxHeight = null;
+              setTimeout(() => Vue.set(this.show, i, this.show[i] =! this.show[i]), 200)
+              document.getElementById(`transform_collapse_${i}`).classList.remove('transform_collapse');
+              document.getElementById(`transform_collapse_${i}`).classList.add('transform_collapse_2');
+            }
+        },
+        btnModal(){
+          this.modal =! this.modal
+        }
+        ,
+        closeModal(){
+          var modal = document.getElementById('myModal');
+          var modal_content = document.getElementsByClassName('modal-content')[0];
+          modal.classList.remove('modal-transition')
+          setTimeout(function(){
+            modal.classList.add('modal')
+            modal_content.classList.remove('modal-content-transition')
+          }, 30)
+        },
+        openModal(){
+          var modal = document.getElementById('myModal');
+          var modal_content = document.getElementsByClassName('modal-content')[0];
+          modal.classList.remove('modal')
+          setTimeout(function(){
+            modal.classList.add('modal-transition')
+            modal_content.classList.add('modal-content-transition')
+          }, 30)
+        }
+      },
+    computed: {
+      classModal: function () {
+        return {
+          'modal': !this.modal,
+          'modal-transition': this.modal,
+        }
+      },
+      classModalContent: function () {
+        return {
+          'modal-content': !this.modal,
+          'modal-content-transition': this.modal,
+        }
+      },
+      classAnimatedContent: function () {
+        return {
+          'animated slideOutUp': !this.modal,
+          'animated slideInDown': this.modal,
+        }
+      },
     },
+     
     components: {
       Datepicker
     }

191fce8f68b340a26d904c96ecf71bc547171f6d
cristian leonardo <cristianleonardo999@gmail.com>
2019-04-09 17:23:07 -0500

    progress in medical record view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,59 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// Read Sprockets README (https://github.com/rails/sprockets//sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require froala_editor.min.js
+// = require plugins/align.min.js
+// = require plugins/char_counter.min.js
+// = require plugins/code_beautifier.min.js
+// = require plugins/code_view.min.js
+// = require plugins/colors.min.js
+// = require plugins/emoticons.min.js
+// = require plugins/entities.min.js
+// = require plugins/file.min.js
+// = require plugins/font_family.min.js
+// = require plugins/font_size.min.js
+// = require plugins/fullscreen.min.js
+// = require plugins/help.min.js
+// = require plugins/image.min.js
+// = require plugins/image_manager.min.js
+// = require plugins/inline_class.min.js
+// = require plugins/inline_style.min.js
+// = require plugins/line_breaker.min.js
+// = require plugins/line_height.min.js
+// = require plugins/link.min.js
+// = require plugins/lists.min.js
+// = require plugins/paragraph_format.min.js
+// = require plugins/paragraph_style.min.js
+// = require plugins/print.min.js
+// = require plugins/quick_insert.min.js
+// = require plugins/quote.min.js
+// = require plugins/save.min.js
+// = require plugins/table.min.js
+// = require plugins/special_characters.min.js
+// = require plugins/url.min.js
+// = require plugins/video.min.js
+
+// = require third_party/embedly.min.js
+// = require third_party/font_awesome.min.js
+// = require third_party/image_aviary.min.js
+// = require third_party/image_tui.min.js
+// = require third_party/spell_checker.min.js
+//= require languages/es.js
+
+$( document ).on('turbolinks:load',function() {
+  setTimeout(function () {
+    $('textarea').froalaEditor({
+      iconsTemplate: "font_awesome_5",
+      theme: 'custom',
+      language: 'es',
+    });
+  }, 200);
+});

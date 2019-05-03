d4344aef169dcd76dce7c9fb4b97e94238bfbc53
cristian leonardo <cristianleonardo999@gmail.com>
2019-05-03 15:36:04 -0500

    add conditional rendering vue instances for id

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -5,86 +5,88 @@ Vue.use(TurbolinksAdapter)
 Vue.use(vClickOutside)
 
 document.addEventListener('turbolinks:load', () => {
-  var app = new Vue({
-    el: '#home_page',
-    data: {
-      show: false,
-      show: [],
-      modal: false,
-      modal1: [],
-      modal2: {},
-    },
-    methods: {
-      modalId(i){
-        Vue.set(this.modal2, i , !this.modal2[i]);
-      }
-      ,
-      close(i) {
-          var a = document.getElementsByClassName('collapse-content')[i];
-          if(a.scrollHeight == 0){
-            Vue.set(this.show, i, this.show[i] =! this.show[i]);
-            setTimeout(function(){a.style.maxHeight = a.scrollHeight + "px"}, 30)
-            document.getElementById(`collapse_${i}`).classList.add('transform_collapse');
-          }else{
+  if(document.getElementById('home_page')) {
+    var app = new Vue({
+      el: '#home_page',
+      data: {
+        show: false,
+        show: [],
+        modal: false,
+        modal1: [],
+        modal2: {},
+      },
+      methods: {
+        modalId(i){
+          Vue.set(this.modal2, i , !this.modal2[i]);
+        }
+        ,
+        close(i) {
+            var a = document.getElementsByClassName('collapse-content')[i];
+            if(a.scrollHeight == 0){
+              Vue.set(this.show, i, this.show[i] =! this.show[i]);
+              setTimeout(function(){a.style.maxHeight = a.scrollHeight + "px"}, 30)
+              document.getElementById(`collapse_${i}`).classList.add('transform_collapse');
+            }else{
+              a.style.maxHeight = null;
+              setTimeout(() => Vue.set(this.show, i, this.show[i] =! this.show[i]), 200)
+              document.getElementById(`collapse_${i}`).classList.remove('transform_collapse');
+              document.getElementById(`collapse_${i}`).classList.add('transform_collapse_2');
+              setTimeout(() => document.getElementById(`collapse_${i}`).classList.remove('transform_collapse_2'), 200)
+            }
+        },
+        close_collapse(i){
+            var a = document.getElementsByClassName('collapse-content')[i];
             a.style.maxHeight = null;
-            setTimeout(() => Vue.set(this.show, i, this.show[i] =! this.show[i]), 200)
-            document.getElementById(`collapse_${i}`).classList.remove('transform_collapse');
-            document.getElementById(`collapse_${i}`).classList.add('transform_collapse_2');
-            setTimeout(() => document.getElementById(`collapse_${i}`).classList.remove('transform_collapse_2'), 200)
+            if (this.show[i]) {
+              setTimeout(() => Vue.set(this.show, i, this.show[i] = false), 200)
+              document.getElementById(`collapse_${i}`).classList.remove('transform_collapse');
+              document.getElementById(`collapse_${i}`).classList.add('transform_collapse_2');
+              setTimeout(() => document.getElementById(`collapse_${i}`).classList.remove('transform_collapse_2'), 200)
+            }
+        },
+        btnModal(){
+          this.modal =! this.modal
+        }
+        ,
+        closeModal(){
+          var modal = document.getElementById('myModal');
+          var modal_content = document.getElementsByClassName('modal-content')[0];
+          modal.classList.remove('modal-transition')
+          setTimeout(function(){
+            modal.classList.add('modal')
+            modal_content.classList.remove('modal-content-transition')
+          }, 30)
+        },
+        openModal(){
+          var modal = document.getElementById('myModal');
+          var modal_content = document.getElementsByClassName('modal-content')[0];
+          modal.classList.remove('modal')
+          setTimeout(function(){
+            modal.classList.add('modal-transition')
+            modal_content.classList.add('modal-content-transition')
+          }, 30)
+        }
+      },
+      computed: {
+        classModal: function () {
+          return {
+            'modal': !this.modal,
+            'modal-transition': this.modal,
           }
-      },
-      close_collapse(i){
-          var a = document.getElementsByClassName('collapse-content')[i];
-          a.style.maxHeight = null;
-          if (this.show[i]) {
-            setTimeout(() => Vue.set(this.show, i, this.show[i] = false), 200)
-            document.getElementById(`collapse_${i}`).classList.remove('transform_collapse');
-            document.getElementById(`collapse_${i}`).classList.add('transform_collapse_2');
-            setTimeout(() => document.getElementById(`collapse_${i}`).classList.remove('transform_collapse_2'), 200)
+        },
+        classModalContent: function () {
+          return {
+            'modal-content': !this.modal,
+            'modal-content-transition': this.modal,
           }
-      },
-      btnModal(){
-        this.modal =! this.modal
+        },
+        classAnimatedContent: function () {
+          return {
+            'animated slideOutUp': !this.modal,
+            'animated slideInDown': this.modal,
+          }
+        },
       }
-      ,
-      closeModal(){
-        var modal = document.getElementById('myModal');
-        var modal_content = document.getElementsByClassName('modal-content')[0];
-        modal.classList.remove('modal-transition')
-        setTimeout(function(){
-          modal.classList.add('modal')
-          modal_content.classList.remove('modal-content-transition')
-        }, 30)
-      },
-      openModal(){
-        var modal = document.getElementById('myModal');
-        var modal_content = document.getElementsByClassName('modal-content')[0];
-        modal.classList.remove('modal')
-        setTimeout(function(){
-          modal.classList.add('modal-transition')
-          modal_content.classList.add('modal-content-transition')
-        }, 30)
-      }
-    },
-    computed: {
-      classModal: function () {
-        return {
-          'modal': !this.modal,
-          'modal-transition': this.modal,
-        }
-      },
-      classModalContent: function () {
-        return {
-          'modal-content': !this.modal,
-          'modal-content-transition': this.modal,
-        }
-      },
-      classAnimatedContent: function () {
-        return {
-          'animated slideOutUp': !this.modal,
-          'animated slideInDown': this.modal,
-        }
-      },
-    }
-  })
+    })
+  }
 })
--- app/javascript/packs/medical_record.js
+++ app/javascript/packs/medical_record.js
@@ -3,58 +3,60 @@ import TurbolinksAdapter from 'vue-turbolinks';
 Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
-  var app = new Vue({
-    el: '#medical_record',
-    data: {
-      show: false,
-      showM: false,
-      showC: false,
-      algo: false,
-      tabItems: {},
-      modal2: {},
-    },
-    methods: {
-      modalId(i){
-        Vue.set(this.modal2, i , !this.modal2[i]);
+  if(document.getElementById('medical_record')) {
+    var app = new Vue({
+      el: '#medical_record',
+      data: {
+        show: false,
+        showM: false,
+        showC: false,
+        algo: false,
+        tabItems: {},
+        modal2: {},
       },
-      showText(){
-        this.show =! this.show
-        setTimeout(function () {
-          tinyMCE.init({
-            selector: 'textarea',
-            plugins: 'paste print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern help',
-            toolbar: 'fullscreen | formatselect | bold italic strikethrough forecolor backcolor permanentpen formatpainter | link image media pageembed | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | removeformat | addcomment',
-            image_advtab: true,
-            paste_data_images: true,
-            width: '100%',
-            height: 409,
-            language: 'es_MX'
-          })
-        }, 10);
+      methods: {
+        modalId(i){
+          Vue.set(this.modal2, i , !this.modal2[i]);
+        },
+        showText(){
+          this.show =! this.show
+          setTimeout(function () {
+            tinyMCE.init({
+              selector: 'textarea',
+              plugins: 'paste print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern help',
+              toolbar: 'fullscreen | formatselect | bold italic strikethrough forecolor backcolor permanentpen formatpainter | link image media pageembed | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | removeformat | addcomment',
+              image_advtab: true,
+              paste_data_images: true,
+              width: '100%',
+              height: 409,
+              language: 'es_MX'
+            })
+          }, 10);
+        },
+        showMedicalRecord(){
+          this.showM =! this.showM
+        }
       },
-      showMedicalRecord(){
-        this.showM =! this.showM
+      computed: {
+        classIconButton: function(){
+          return {
+            'fa-plus btn-primary': !this.show,
+            'fa-trash btn-red-light': this.show
+          }
+        },
+        transformClass: function(){
+          return{
+            'transform_collapse_2': !this.showC,
+            'transform_collapse': this.showC,
+          }
+        },
+        animatedClass: function(){
+          return{
+            'animated slideOutUp': !this.showC,
+            'animated slideInDown': this.showC,
+          }
+        }
       }
-    },
-    computed: {
-      classIconButton: function(){
-        return {
-          'fa-plus btn-primary': !this.show,
-          'fa-trash btn-red-light': this.show
-        }
-      },
-      transformClass: function(){
-        return{
-          'transform_collapse_2': !this.showC,
-          'transform_collapse': this.showC,
-        }
-      },
-      animatedClass: function(){
-        return{
-          'animated slideOutUp': !this.showC,
-          'animated slideInDown': this.showC,
-        }
-      }
-    }
-  })
+    })
+  }
 })

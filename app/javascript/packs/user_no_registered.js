fdf753d75fd83fba3d873f8d7765cab5e59fda0a
cristian leonardo <cristianleonardo999@gmail.com>
2019-05-24 17:47:52 -0500

    Refactorign code javascript in a mixin for the calendar module

--- app/javascript/packs/user_no_registered.js
+++ app/javascript/packs/user_no_registered.js
@@ -2,22 +2,19 @@ import Vue from 'vue/dist/vue.esm'
 import Vuelidate from 'vuelidate'
 import Datepicker from 'vuejs-datepicker';
 import TurbolinksAdapter from 'vue-turbolinks';
-import VueMoment from 'vue-moment';
 import VueResource from 'vue-resource'
-import moment from 'moment';
 import {en, es} from 'vuejs-datepicker/dist/locale'
 import { required, email, minLength, sameAs } from 'vuelidate/lib/validators';
 import Ripple from 'vue-ripple-directive'
+import { CalendarMixin } from './mixins/calendar_mixin'
 Vue.directive('ripple', Ripple);
 Vue.use(TurbolinksAdapter)
 Vue.use(Vuelidate)
 Vue.use(VueResource)
-Vue.use(VueMoment, { moment } );
-moment.locale('es')
 
 document.addEventListener('turbolinks:load', () => {
   if(document.getElementById('user_no_registered')) {
-  var module = require('colombia-holidays');
+
   var procedure_type = JSON.parse(document.getElementById("user_no_registered").getAttribute('procedure_types'))[0]
   var procedure_name = procedure_type != null ? procedure_type.procedure_type_name : ''
   var procedure_duration = procedure_type != null ? procedure_type.procedure_duration : ''
@@ -25,6 +22,7 @@ document.addEventListener('turbolinks:load', () => {
 
   var app = new Vue({
     el: '#user_no_registered',
+    mixins: [CalendarMixin],
     data: {
       emailValue: '',
       firstNameValue: '',
@@ -32,30 +30,17 @@ document.addEventListener('turbolinks:load', () => {
       passwordValue: '',
       passwordConfirmationValue: '',
       birthdateValue: document.getElementById("user_no_registered").getAttribute('birthdate'),
-      appointmentHour: '',
       idNumber: '',
       idType: '',
       phoneNumber: '',
       procedureName: procedure_name,
       procedureDuration: procedure_duration,
-      doctor: "",
-      doctorWorkingWeek: '',
       doctorId: document.getElementById("user_no_registered").getAttribute('doctor_id'),
       procedureTypeId: procedure_id,
-      weekDays: ['Lunes','Martes','Miercoles','Jueves','Viernes'],
-      hours: [],
-      workingHours: [],
-      unavailableWorkingHours: "",
-      schedule: [],
       scheduleIndexs: [],
-      holidays: module.getColombiaHolidaysByYear(2019),
-      indexWeek: 0,
-      week: [],
-      hiddenDropdown: false,
       indexNav: 0,
       navIds: [1,2,3],
       currentNav: "",
-      someData: "",
       patient: '',
       en: en,
       es: es
@@ -96,14 +81,6 @@ document.addEventListener('turbolinks:load', () => {
           el.classList.replace('invalid_form', 'focus_form')
         }
       },
-      fetchData: function(){
-        this.$http.get(`/api/appointments/fetch_appointment_data/${this.doctorId}/${this.procedureTypeId}`).then(response => {
-          this.doctor = JSON.parse(response.body.doctor)[0];
-          this.unavailableWorkingHours = JSON.parse(response.body.unavailable_working_hours);
-          this.doctorWorkingWeek = this.doctor.doctor_working_weeks[this.indexWeek]
-          this.matrixWorkingHours()
-        }, response => { console.log(response) });
-      },
       fetchUser: function(){
         this.$http.get(`/api/appointments/fetch_user/${this.idNumber}/${this.idType}`).then(response => {
           this.patient = response.body[0]
@@ -112,76 +89,7 @@ document.addEventListener('turbolinks:load', () => {
           this.phoneNumber = this.patient != null ? this.patient.phone_number : ''
           this.emailValue = this.patient != null ? this.patient.email : ''
         }, response => { console.log(response) });
-      }
-      ,
-      selectHour: function(){
-        var a = event.currentTarget.attributes.id.value
-        this.appointmentHour = a
       },
-      selectWorkingHour: function(working_day, hour){
-        var arrayHours = []
-        var date = working_day.working_date
-        var datetime = moment(`${date} ${hour}`)
-        if (datetime.isAfter(moment().add(23, 'hour'))) {
-          var limit_datetime = moment(`${date} ${hour}`).add(1,'hour')
-          var wh = working_day.working_hours
-          for (var i = 0; i < wh.length; i++) {
-            let initial_hour = moment(wh[i].initial_hour)
-            let end_hour = moment(wh[i].end_hour)
-            if (datetime.isBetween(initial_hour, end_hour, null, '[)')) {
-              initial_hour = datetime
-              while (initial_hour.isBefore(end_hour) && initial_hour.isBefore(limit_datetime)) {
-                if (this.isAvailableHour(initial_hour) && initial_hour.isAfter(moment().add(1, 'day'))) {
-                  arrayHours.push(initial_hour.format())
-                }
-                initial_hour.add(this.procedureDuration,'minutes')
-              }
-              break;
-            }
-          }
-        }
-        return arrayHours;
-      },
-      isAvailableHour: function(appointment_datetime){
-        return this.unavailableWorkingHours.findIndex(d => moment(d.appointment_datetime).isSame(appointment_datetime)) == -1
-      },
-      isHoliday: function(working_date){
-        return this.holidays.findIndex(h => moment(h.holiday).isSame(moment(working_date))) != -1
-      }
-      ,
-      matrixWorkingHours: function(){
-        var working_days = this.doctorWorkingWeek.working_days
-        if (working_days.length == 0 || !this.renderForm) {
-          this.workingHours = Array(5).fill(null).map(()=>Array(this.hours.length).fill([]))
-        }
-        else {
-          this.createWeek()
-          for (var i = 0; i < this.week.length; i++) {
-            this.workingHours[i] = []
-            this.schedule [i] = []
-            var wdays = working_days.find(wd => moment(wd.working_date).isSame(this.week[i]))
-            for (var j = 0; j < this.hours.length; j++) {
-              var wh = []
-              if (wdays != null) {
-                wh = !this.isHoliday(wdays.working_date) ? this.selectWorkingHour(wdays, this.hours[j]) : []
-              }
-              this.workingHours[i][j] = wh
-              this.schedule [i][j] = Array(wh.length).fill(false)
-            }
-          }
-        }
-      },
-      showOptions: function(i, j, k){
-        var schedule = this.schedule
-        schedule[i][j][k] =! this.schedule[i][j][k]
-        Vue.set(this.schedule, i, schedule[i])
-      },
-      createWeek: function(){
-        for (var i = 0; i < 5; i++) {
-          Vue.set(this.week, i, moment(this.doctorWorkingWeek.initial_date).day(i + 1))
-        }
-      }
-      ,
       changeNav: function(){
         this.indexNav += 1
         this.currentNav = this.navIds[this.indexNav]
@@ -218,10 +126,6 @@ document.addEventListener('turbolinks:load', () => {
     }
     ,
     watch: {
-     doctorId(){
-       this.appointmentHour = ''
-       this.fetchData()
-     },
      schedule(){},
      idNumber(){
        if (this.idType != "") {
@@ -232,10 +136,6 @@ document.addEventListener('turbolinks:load', () => {
        if (this.idNumber != "") {
          this.fetchUser()
        }
-     },
-     indexWeek(){
-       this.doctorWorkingWeek = this.doctor.doctor_working_weeks[this.indexWeek]
-       this.matrixWorkingHours()
      },
      currentNav(){
        this.indexNav = this.navIds.findIndex(n => n == this.currentNav)
@@ -254,24 +154,6 @@ document.addEventListener('turbolinks:load', () => {
     },
     components: {
       Datepicker
-    },
-    mounted: function () {
-      this.$nextTick(function () {
-        var hours = []
-        var format = ["am","pm"]
-        for (var i in format) {
-          for (var j = 0; j < 6; j++) {
-            let v = 6 + j
-            v = (format[i] === "am") ? v : (v - 5)
-            hours.push(`${v < 10 ? 0 : ''}${v}:00 ${format[i]}`)
-            if (v == 11) {
-              hours.push("12:00 pm")
-            }
-          }
-        }
-        this.hours = hours
-        this.fetchData()
-      })
     }
   })
 }})

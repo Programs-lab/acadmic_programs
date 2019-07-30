efccce34c1019f25bab9a5b8df15423687f930b7
JoDaBaRo <josdabayona@gmail.com>
2019-07-30 12:07:30 -0500

    Final details for signature feature

--- app/uploaders/signature_uploader.rb
+++ app/uploaders/signature_uploader.rb
@@ -30,7 +30,11 @@ class SignatureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [300, 140]
+    process resize_to_fill: [0, 100]
+  end
+
+  version :full do
+    process resize_to_fill: [0, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.

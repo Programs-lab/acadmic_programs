1e65f8272943aa878043df9a558deda78a014be9
JoDaBaRo <josdabayona@gmail.com>
2019-08-08 09:52:24 -0500

    Possible fix to sidekiq cron not running

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,16 @@
-schedule_file = "config/schedule.yml"
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+
+
+def sidekiq_cron
+  schedule_file = "config/schedule.yml"
+  if File.exist?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
 
+
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 7, url: ENV.fetch("REDISTOGO_URL", "redis://localhost:6379"), namespace: "Gastromed_sidekiq_#{Rails.env}" }
+  config.redis = { :size => 12, url: ENV.fetch("REDISTOGO_URL", "redis://localhost:6379"), namespace: "Gastromed_sidekiq_#{Rails.env}" }
+  sidekiq_cron
 end
 
 Sidekiq.configure_client do |config|

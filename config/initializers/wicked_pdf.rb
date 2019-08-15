30cf61ee7d15687d8694f204d5bac0d094328205
JoDaBaRo <josdabayona@gmail.com>
2019-08-15 16:52:41 -0500

    removes exe path config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,7 +8,4 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
   WickedPdf.config ||= {}
-  WickedPdf.config.merge!({
-    :exe_path => "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-heroku-#{Gem.loaded_specs['wkhtmltopdf-heroku'].version}/bin/wkhtmltopdf"
-  })
 

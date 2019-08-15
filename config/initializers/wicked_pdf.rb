70fe5b6a846b1e787794516511b96c5a533f8a41
JoDaBaRo <josdabayona@gmail.com>
2019-08-15 16:18:33 -0500

    another try

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,6 +9,6 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
   WickedPdf.config ||= {}
   WickedPdf.config.merge!({
-    :exe_path => "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/wkhtmltopdf"
+    :exe_path => "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-edge-#{Gem.loaded_specs['wkhtmltopdf-binary-edge'].version}/bin/wkhtmltopdf"
   })
 

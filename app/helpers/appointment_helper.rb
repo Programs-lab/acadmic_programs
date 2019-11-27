35134a6657f26f6fd6e01809670e399759724e34
JoDaBaRo <josdabayona@gmail.com>
2019-11-27 08:08:12 -0500

    adds badges

--- app/helpers/appointment_helper.rb
+++ app/helpers/appointment_helper.rb
@@ -1,16 +1,17 @@
 module AppointmentHelper
 
-  def appointment_state(appointment)
-    if appointment.appointment_datetime < DateTime.now && (!appointment.completed? && !appointment.attended? && !appointment.canceled?)
-      return ["Vencida", "badge-default"]
+  def process_state(process)
+
+    if process + 1.month == Date.today
+      ["Alerta", "badge-warning"]
+    elsif process + 1.week == Date.today
+        ["Alerta", "badge-danger"]
+    elsif process <= Date.today      
+        ["Vencido", "badge-default"]
     else
-      case appointment.state
-      when "pending" then ["Pendiente", "badge-warning"]
-      when "completed" then ["Completada", "badge-success"]
-      when "attended" then ["Cumplida", "badge-info"]
-      when "canceled" then  ["Cancelada", "badge-danger "]
-      end
-    end      
+        ["Vigente", "badge-success"]
+    end
+
   end
 
   def disabled_button(appointment)
